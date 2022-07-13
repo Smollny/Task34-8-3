feb2a4bded80fb030482ebd472df3d3e5820af33
Smollny <smollny@mail.ru>
2022-07-13 23:45:57 +0300

    upload all files

---
+++ HomeApi.Data/Queries/UpdateRoomQuery.cs
@@ -0,0 +1,21 @@
+﻿namespace HomeApi.Data.Queries
+{
+    /// <summary>
+    /// Класс для передачи дополнительных параметров при обновлении комнаты
+    /// </summary>
+    public class UpdateRoomQuery
+    {
+        public string NewName { get; }
+        public int NewArea { get;  }
+        public bool NewGasConnected { get;  }
+        public int NewVoltage { get; }
+
+        public UpdateRoomQuery(string newName = null, int newArea = 0, bool newGasConnection=false, int newVoltage = 0)
+        {
+            NewName = newName;
+            NewArea = newArea;
+            NewGasConnected = newGasConnection;
+            NewVoltage = newVoltage;
+        }
+    }
+}
